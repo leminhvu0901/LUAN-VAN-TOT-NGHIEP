75645504b83d7184cee8a3b00c9f3be13a034d69
Le Minh Vu <leminhvu9124@gmail.com>
2026-07-30 19:06:31 +0700

    docs: add explanatory comments to Banner model

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Banner extends Model
 {
-    protected $table = 'banners';
-    protected $guarded = [];
+    protected $table = 'banners'; // Model này đại diện và liên kết trực tiếp với bảng banners trong DB
+    protected $guarded = []; // Cho phép điền hàng loạt cho tất cả các trường (không khóa trường nào)
 }
